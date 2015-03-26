e9fe625c126e44dd6da865d14793257a16837426
Victor Nazarov <asviraspossible@gmail.com>
2015-03-26 12:45:42 +0300

    Final release script

--- release.sh
+++ release.sh
@@ -19,19 +19,24 @@ echo " 2. Beta"
 echo " 3. Release Candidate"
 echo " 4. Final"
 
+echo
 echo "Choose variant: "
 read N
+echo
 KIND=$(echo alpha beta rc final| awk -v "N=$N" '{print $N}')
 
 N_MAX=$(echo "$VERSION $KIND" | awk -f version.awk | wc -l)
 echo "$VERSION $KIND" | awk -f version.awk | awk '{print ++n ". " $0} END{print ++n ". Custom" }'
 
+echo
 echo "Choose variant: "
 read N
+echo
 
 if test "$N" -lt 1 || test "$N" -gt "$N_MAX"; then
   echo "Enter custom version number:"
   read RELEASE_VERSION
+  echo
 else
   RELEASE_VERSION=$(echo "$VERSION $KIND" | awk -f version.awk | head -n "$N" | tail -n 1)
 fi
@@ -54,7 +59,14 @@ if ! test -z "$UNSTABLE_VERSION"; then
   echo "UNSTABLE_VERSION=$UNSTABLE_VERSION" >> versions.sh.next
 fi
 
-mvn release:prepare "-DdryRun=true" "-DreleaseVersion=$RELEASE_VERSION" "-DdevelopmentVersion=$NEXT_DEVELOPMENT_VERSION" \
-	&& mvn release:clean \
-	&& ./update-versions.sh \
-	&& mvn release:prepare "-DreleaseVersion=$RELEASE_VERSION" "-DdevelopmentVersion=$NEXT_DEVELOPMENT_VERSION"
+if mvn release:prepare "-DdryRun=true" "-DreleaseVersion=$RELEASE_VERSION" "-DdevelopmentVersion=$NEXT_DEVELOPMENT_VERSION"; then
+  echo "Is everything ok? Proceed with the release? [yes/no]:"
+  read ANSWER
+  if test "$ANSWER" '=' "yes"; then
+    mvn release:clean \
+	&& mvn release:prepare "-DreleaseVersion=$RELEASE_VERSION" "-DdevelopmentVersion=$NEXT_DEVELOPMENT_VERSION" \
+        && mvn release:perform \
+        && ./update-versions.sh \
+        && git push
+  fi
+fi
